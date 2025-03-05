ffffceea4dbc031dbe540df3964f985ae191c476
Jefté Lima <jefte_lima@hotmail.com>
2025-03-05 15:19:34 -0300

    test: add Transaction component rendering test
    - Create Transaction.test.js to verify transaction rendering
    - Test transaction type and value updates
    - Ensure Transaction component displays correct information dynamically

---
+++ src/components/Statement/Transaction/Transaction.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import Transaction from '.'
+import styles from './Transaction.module.css'
+
+test('Should render transaction type and value with updated props', () => {
+	const transaction = {
+		type: 'Transaction',
+		value: 100
+	}
+	const { rerender } = render(<Transaction transaction={transaction} styles={styles} />)
+	const transactionType = screen.getByTestId('transactionType')
+	const transactionValue = screen.getByTestId('transactionValue')
+
+	expect(transactionType).toHaveTextContent('Transaction')
+	expect(transactionValue).toHaveTextContent('- $ 100')
+
+	const newTransaction = {
+		type: 'Deposit',
+		value: 500
+	}
+	rerender(<Transaction transaction={newTransaction} styles={styles} />)
+	const newTransactionType = screen.getByTestId('transactionType')
+	const newTransactionValue = screen.getByTestId('transactionValue')
+
+	expect(newTransactionType).toHaveTextContent('Deposit')
+	expect(newTransactionValue).toHaveTextContent('$ 500')
+})
